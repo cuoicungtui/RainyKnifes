3a6e9e9457d52ce39aaae7fcb65e53fbd13149f6
cuoicungtui <tuongdangvuongquoc901@gmail.com>
2022-05-10 13:20:52 +0700

    Firt

---
+++ Rainy Knifes/Assets/Scrips/PlayerMovie.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMovie : MonoBehaviour
+{
+    private Animator anim;
+
+    private SpriteRenderer sr;
+    
+    private float speed = 3f;
+
+    private float Min_X = -1.9f;
+
+    private float Max_X = 3.86f;
+
+    public Text Timer_text;
+
+    private int timer;
+  
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+
+        anim = GetComponent<Animator>();
+
+        sr = GetComponent<SpriteRenderer>();
+
+        
+    }
+
+    void Start()
+    {
+        Time.timeScale = 1f;
+
+        StartCoroutine(CountTime());
+
+        timer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        PlayerBound();
+    }
+
+
+    void PlayerBound()
+    {
+        Vector3 temp = transform.position;
+
+        if(temp.x < Min_X)
+        {
+            temp.x = Min_X;
+
+        }
+
+        if (temp.x > Max_X)
+        {
+            temp.x = Min_X;
+
+        }
+
+        transform.position = temp;
+
+    }
+
+    void Move()
+    {
+
+        float h = Input.GetAxisRaw("Horizontal");
+
+
+        //Debug.Log("Text: " + h);
+
+        Vector3 temp = transform.position;
+
+        if (h > 0)
+        {
+            temp.x += speed * Time.deltaTime;
+
+            sr.flipX = false;
+
+            anim.SetBool("Walk", true);
+        }
+        else if(h<0)
+        {
+
+            temp.x -= speed * Time.deltaTime;
+
+            sr.flipX = true;
+
+            anim.SetBool("Walk", true);
+
+        }
+        else if (h == 0)
+        {
+            anim.SetBool("Walk", false);
+        }
+
+      //  temp.x += speed * Time.deltaTime;
+
+        transform.position = temp;
+
+    }
+
+
+    IEnumerator CountTime()
+    {
+
+        yield return new WaitForSeconds(1f);
+
+        timer++;
+
+        Timer_text.text = "Timer : " + timer;
+
+        StartCoroutine(CountTime());
+             
+    }
+
+    IEnumerator RestartGame()
+    {
+        yield return new WaitForSecondsRealtime(2f);
+
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D target)
+    {
+       if(target.tag == "Knife")
+        {
+
+            Time.timeScale = 0f;
+
+            StartCoroutine(RestartGame());
+        }
+
+
+    }
+}
---
+++ Rainy Knifes/Assets/Scrips/Spawner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    public GameObject Knife;
+
+    private float Min_X  = -1.9f;
+
+    private float Max_X = 3.86f;
+
+
+    public float Min_t = 0.2f;
+
+    public float Max_t = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(StartSpawning());
+        
+    }
+
+    IEnumerator StartSpawning()
+    {
+        yield return new WaitForSeconds(Random.Range(Min_t, Max_t));
+
+        GameObject k = Instantiate(Knife);
+
+        float x = Random.Range(Min_X, Max_X);
+
+        k.transform.position = new Vector2(x, transform.position.y);
+
+        Destroy(k, 5f);
+
+       // Debug.Log("x = "+ x);
+
+        StartCoroutine(StartSpawning());
+
+    }
+}
